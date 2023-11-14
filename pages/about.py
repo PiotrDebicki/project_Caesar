b30ad92569dbea96f2b2d87af9d80848326825a3
PiotrDebicki <piotr@debicki.wroclaw.pl>
2023-11-14 09:21:15 +0100

    changes of about section

--- pages/about.py
+++ pages/about.py
@@ -1,20 +1,22 @@
-from tkinter import Tk, Label
+from tkinter import Tk, Label, Button
 
 class AboutPage(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.title("Ceaser cipher encoder/decoder" )
-        self.app_name = "Ceaser cipher encoder/decoder" 
+        self.eval('tk::PlaceWindow . center')
+        self.resizable(False, False)
+        self.title("Ceaser cipher Encrypter/Decrypter" )
+        self.app_name = "Ceaser cipher Encrypter/Decrypter" 
         self.app_version = "0.1.1"
         self.app_url = "https://github.com/PiotrDebicki/project_Caesar"
         self.app_ = "Copyright © 2018 Piotr Debicki.\nAll rights reserved."
-        self.init_page()
 
     def init_page(self):
         Label(self, text=self.app_name).pack()
         Label(self, text=self.app_version).pack()
         Label(self, text=self.app_url).pack()
         Label(self, text=self.app_).pack()
+        Button(self, text="Close", command=lambda: self.destroy()).pack()
 
 if __name__ == "__main__":
     APP_ABOUT = AboutPage()
