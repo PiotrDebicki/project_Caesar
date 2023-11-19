2e78244d20c720b317035ee2dc13a6b55943a47b
PiotrDebicki <piotr@debicki.wroclaw.pl>
2023-11-19 23:01:31 +0100

    patch of encrypting function

--- functions/encrypt.py
+++ functions/encrypt.py
@@ -1,12 +1,14 @@
 def encrypt(text:str, shift:int):
   encrypted:str = ""
+  while("\n" in text):
+    text = text.replace("\n", " ")
   for char in text:
     if char.isalpha():
       ascii_code = ord(char)
       if(ascii_code <=90):
-        ascii_code = (ascii_code - 64 + shift)%26 + 64
+        ascii_code = (ascii_code - 65 + shift)%26 + 65
       else:
-        ascii_code = (ascii_code - 96 + shift)%26 + 96
+        ascii_code = (ascii_code - 97 + shift)%26 + 97
       encrypted += chr(ascii_code)
     else:
       encrypted+=char
